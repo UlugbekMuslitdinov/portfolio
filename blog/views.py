382903ff4a5143981aee0d6ea90a01d0d3ea6f44
Ulugbek <ulugbekmuslitdinov@gmail.com>
2021-07-22 00:18:58 +0500

    search

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.db.models import Q
 from .models import *
 
 
@@ -14,8 +15,8 @@ def skills(request):
 
 def articles_list(request):
     """List of all articles sorted by the date created"""
-    artticles = Article.objects.all().order_by('-created_at')
-    context = {'articles': artticles}
+    articles = Article.objects.all().order_by('-created_at')
+    context = {'articles': articles}
     return render(request, 'articles.html', context)
 
 
@@ -24,3 +25,11 @@ def article_detail(request, article_slug):
     article = Article.objects.get(slug=article_slug)
     context = {'article': article}
     return render(request, 'article_detail.html', context)
+
+def search_results(request):
+    query = request.GET.get('q')
+    object_list = Article.objects.filter(
+        Q(topic__icontains=query) | Q(text__icontains=query) | Q(summary__icontains=query) | Q(author__icontains=query)
+    )
+    context = {'object_list': object_list}
+    return render(request, 'search_results.html', context)
