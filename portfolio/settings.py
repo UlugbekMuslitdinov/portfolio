c9be77ecdd70736e307299849e013b71251254e3
Ulugbek <ulugbekmuslitdinov@gmail.com>
2021-07-22 11:05:52 +0500

    deployment
    Gunicorn,dj-databse-url, gunicorn, heroku.yml

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -161,3 +161,9 @@ if ENVIRONMENT == 'production':
     SECURE_CONTENT_TYPE_NOSNIFF = True
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
+
+
+# Heroku
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
