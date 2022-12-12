eacc3a564a21b035c8bdbaadb1a7e0f8431999b1
Ulugbek <ulugbekmuslitdinov@gmail.com>
2022-12-11 19:12:22 -0700

    added allowed hostst

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 ENVIRONMENT = os.environ.get('ENVIRONMENT', default='development')
-ALLOWED_HOSTS = ['umuslitdinov.dev', 'www.umuslitdinov.dev', 'umuslitdinov.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['umuslitdinov.dev', 'www.umuslitdinov.dev', 'umuslitdinov.herokuapp.com', 'localhost', '127.0.0.1', '185.196.213.35']
 
 
 # Application definition
