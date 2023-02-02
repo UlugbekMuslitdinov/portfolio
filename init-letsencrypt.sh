0dae22313f41d9c7e3218d4be5afeee2dceea743
Ulugbek Muslitdinov <ulugbekmuslitdinov@gmail.com>
2023-02-02 02:42:37 +0000

    new letsencrypt.sh

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,10 +5,10 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(umuslitdinov.dev www.umuslitdinov.dev)
+domains=(example.org www.example.org)
 rsa_key_size=4096
 data_path="./data/certbot"
-email="ulugbekmuslitdinov@gmail.com" # Adding a valid address is strongly recommended
+email="" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
