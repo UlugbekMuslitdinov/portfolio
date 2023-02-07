99f87ccf791618db608076a5ed2ce675fd746989
Ulugbek <ulugbekmuslitdinov@gmail.com>
2023-02-06 22:26:43 -0700

    deleted blog

--- blog/admin.py
+++ blog/admin.py
@@ -2,14 +2,8 @@ from django.contrib import admin
 from .models import *
 
 
-class ArticleAdmin(admin.ModelAdmin):
-    prepopulated_fields = {"slug": ("topic",)}
-    list_display = ('topic', 'author', 'created_at')
-
-
 class SkillsAdmin(admin.ModelAdmin):
     list_display = ('skill', 'logo_url')
 
 
-admin.site.register(Article, ArticleAdmin)
 admin.site.register(Skills, SkillsAdmin)
--- blog/urls.py
+++ blog/urls.py
@@ -3,9 +3,6 @@ from .views import *
 app_name = 'blog'
 
 urlpatterns = [
-    path('search/', search_results, name='search_results'),
-    path('blog/<str:article_slug>/', article_detail, name='article_detail'),
-    path('blog/', articles_list, name='articles_list'),
     path('skills/', skills, name='skills'),
     path('projects/', projects, name='projects'),
     path('', home, name='home'),
